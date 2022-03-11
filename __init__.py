abbc2118f9c2974865a0b83d7fc65ab468b92ec6
pocket-sun <chlsun60.163.com>
2022-03-11 21:17:08 +0800

    forced

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ from .read import read
 from .util import line_diag
 from .util import cor_diag
 from .util import hist_diag
-from .get_vol import get_vol
+from .get_ldvol import get_ldvol
 from .get_inte import get_inte
